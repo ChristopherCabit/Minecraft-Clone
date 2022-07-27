e3727b01ac2efa78c4b8d2bfb59d2096bf81f832
BedWarsNoob <68874503+BedWarsNoob@users.noreply.github.com>
2022-07-27 11:45:48 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,12 +1,5 @@
 //blah blah blah
-//Repository
-echo "# Minecraftig" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/BedWarsNoob/Minecraftig.git
-git push -u origin main
+
 
 WNDCLASS wc = { };
 
