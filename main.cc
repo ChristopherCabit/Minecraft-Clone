c9fa894bd7181e739bd00fdb6b9ac278f7b2327e
BedWarsNoob <68874503+BedWarsNoob@users.noreply.github.com>
2022-07-27 11:15:51 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,2 +1,8 @@
 //blah blah blah
+const wchar_t CLASS_NAME[]  = L"Sample Window Class";
 
+WNDCLASS wc = { };
+
+wc.lpfnWndProc   = WindowProc;
+wc.hInstance     = hInstance;
+wc.lpszClassName = CLASS_NAME;
